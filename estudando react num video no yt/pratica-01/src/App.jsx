b6bd3248edeb96332e47d8d26ccbdfe37872a1fc
Kaue Batista <83407339+KaueB@users.noreply.github.com>
2021-07-26 12:28:21 -0300

    pratica 1 terminada

--- estudando react num video no yt/pratica-01/src/App.jsx
+++ estudando react num video no yt/pratica-01/src/App.jsx
@@ -1,5 +1,6 @@
-import React, {useState} from 'react';
-// import {v4 as uuidv4 } from "uuid";
+import React, {useEffect, useState} from 'react';
+import {v4 as uuidv4 } from "uuid";
+import axios from 'axios';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Header from "./components/Header"
@@ -25,6 +26,16 @@ const App = () => {
     },  
   ]);
 
+  useEffect (() => {
+    const fetchTasks = async () => {
+      const { data } = await axios.get('https://jsonplaceholder.cypress.io/todos?_limit=10')
+    
+      setTasks( data );
+    };
+
+    fetchTasks();
+  }, []);
+
   const handleTaskClick = (taskId) => {
     const newTasks = tasks.map((task) => {
       if (task.id === taskId) return {...task, completed: !task.completed };
@@ -39,7 +50,7 @@ const App = () => {
     const newTasks = [ ...tasks, 
       {
         title: taskTitle,
-        id: Math.random(10),
+        id: uuidv4(),
         completed: false,
       },
     ];
--- estudando react num video no yt/pratica-01/src/components/TaskDetails.jsx
+++ estudando react num video no yt/pratica-01/src/components/TaskDetails.jsx
@@ -1,30 +1,32 @@
-import React from 'react';
-import { useHistory, useParams} from "react-router-dom"
+import React from "react";
+import { useHistory, useParams } from "react-router-dom";
 
-import "./Button.css";
+import Button from "./Button";
 
 import "./TaskDetails.css";
 
 const TaskDetails = () => {
+	const params = useParams();
+	const history = useHistory();
 
-    const params = useParams(); 
-    const history = useHistory();
+	const handleBackButtonClick = () => {
+		history.goBack();
+	};
 
-    const handleBackButtonClick = () => {
-        history.goBack();
-    } 
+	return (
+		<>
+			<div className="back-button-container">
+				<Button onClick={handleBackButtonClick}>Voltar</Button>
+			</div>
+			<div className="task-details-container">
+				<h2>{params.taskTitle}</h2>
+				<p>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur
+					minima eius magnam culpa sequi explicabo.
+				</p>
+			</div>
+		</>
+	);
+};
 
-    return (
-        <>
-            <div className="back-button-container">
-                <button className='button' onClick={handleBackButtonClick}>Voltar</button>
-            </div>
-            <div className="task-details-container">
-                <h2>{params.taskTitle}</h2>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis illo eveniet deserunt nesciunt excepturi aspernatur ipsam culpa atque. Dolorum commodi quis corrupti deleniti quae! Ut iure dolores reprehenderit impedit corrupti.</p>
-            </div>
-        </>
-    );
-}
- 
 export default TaskDetails;
