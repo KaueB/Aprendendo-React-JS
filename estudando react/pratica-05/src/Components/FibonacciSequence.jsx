7ce430c4471f3d503121a3e163cbe23ad1b83004
Kaue Batista da Silva <83407339+KaueB@users.noreply.github.com>
2022-07-09 13:24:23 -0300

    a

--- estudando react/pratica-05/src/Components/FibonacciSequence.jsx
+++ estudando react/pratica-05/src/Components/FibonacciSequence.jsx
@@ -4,18 +4,47 @@ import './style/FibonacciSequence.css'
 import '../index.css';
 import '../App.css';
 
-function FibonacciSequence() {
-    const result = useState('Resultado')
+function FibonacciSequence(props) {
+    const [number, setNumber] = useState(null)
+    const [result, setResult] = useState('Resultado')
 
-    function finalResult() {
+    const fibonacci = (num, memo) => {
         
+    }
+
+    function handleResult() {
+        if (number <= 1 ) {
+            setResult('0 1')
+        } else if ( number >=500  ) {
+            setResult('Escolher um número menor')
+        } else {
+            let penultimateNumber = 0
+            let lastNumber = 1
+            let endAnswer = 0
+
+            if (number <= 2 ) {
+                if( number <= 2 ) {
+                    endAnswer = number - 1
+                } else {
+                    var count = 3
+                    while (count <= number) {
+                    endAnswer = lastNumber + penultimateNumber;
+                    penultimateNumber = lastNumber;
+                    lastNumber = endAnswer;
+                    count++;
+                    }
+                }
+                setResult(endAnswer)
+            }
+            
+        }
     }
     
     return (
         <div>
             <h1> Escolhe o número final </h1>
-            <input type='Number' id='number'/>
-            <button onclick="finalResult()">Iniciar</button>
+                <input type='number' min={0} max={500} onChange={e => setNumber(e.target.value)}/>
+                <button onClick={handleResult} >Iniciar</button>
             <h1>{result}</h1>
         </div>
     );
