c1c7ffb83c7e3fed6d658a2e238a7a00a4b21ead
Kaue Batista da Silva <83407339+KaueB@users.noreply.github.com>
2022-07-09 19:33:28 -0300

    a

--- estudando react/pratica-05/src/Components/FibonacciSequence.jsx
+++ estudando react/pratica-05/src/Components/FibonacciSequence.jsx
@@ -8,10 +8,7 @@ function FibonacciSequence(props) {
     const [number, setNumber] = useState(null)
     const [result, setResult] = useState('Resultado')
 
-    const fibonacci = (num, memo) => {
-        
-    }
-
+    /* 
     function handleResult() {
         if (number <= 1 ) {
             setResult('0 1')
@@ -22,24 +19,39 @@ function FibonacciSequence(props) {
             let lastNumber = 1
             let endAnswer = 0
 
-            if (number <= 2 ) {
-                if( number <= 2 ) {
-                    endAnswer = number - 1
-                } else {
-                    var count = 3
-                    while (count <= number) {
-                    endAnswer = lastNumber + penultimateNumber;
-                    penultimateNumber = lastNumber;
-                    lastNumber = endAnswer;
-                    count++;
-                    }
-                }
-                setResult(endAnswer)
-            }
-            
+            let count = 3
+            do {
+                endAnswer = lastNumber + penultimateNumber;
+                penultimateNumber = lastNumber;
+                lastNumber = endAnswer;
+                count++;
+            } while (count <= number);
+            setResult(endAnswer)
         }
-    }
-    
+    } 
+    */
+
+    function handleResult() {
+        if (number < 1 ) {
+            setResult('0 1')
+        } else if ( number >= 500  ) {
+            setResult('Escolher um número menor')
+        } else {
+            let penultimateNumber = 0
+            let lastNumber = 1
+            let endAnswer = 0
+            let resultFib = '0 1'
+
+            while (endAnswer <= number - 1) {
+                endAnswer = lastNumber + penultimateNumber;
+                resultFib += (' ' + endAnswer + '')
+                penultimateNumber = lastNumber;
+                lastNumber = endAnswer;
+            };
+            setResult(resultFib)
+        }
+    } 
+
     return (
         <div>
             <h1> Escolhe o número final </h1>
